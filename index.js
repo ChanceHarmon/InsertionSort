345d209af10c58caba14bcda8bb8360501cd2902
Chance Harmon <henrikillsit@gmail.com>
2019-10-01 15:57:00 -0700

    started

---
+++ index.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const array = [4, 5, 3, 8, 12, 10];
+
+const insertionSort = array => {
+  for (let i = 1; i < array.length; i++) {
+    let current = array[i];
+    let j = i - 1;
+
+    while (j >= 0 && array[j] > current) {
+      array[j + 1] = array[j];
+      j--;
+    }
+    array[j + 1] = current;
+  }
+  return array;
+}
+insertionSort(array);
+
+console.log(array); 
